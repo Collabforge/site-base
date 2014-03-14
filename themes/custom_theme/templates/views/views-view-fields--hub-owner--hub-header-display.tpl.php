bb6085fe25d168e30ba3f245a062c2e7091b2693
summerscope <summerscope@gmail.com>
2014-03-14 15:21:05 +1100

    Tweaks to black heaviness

--- themes/custom_theme/templates/views/views-view-fields--hub-owner--hub-header-display.tpl.php
+++ themes/custom_theme/templates/views/views-view-fields--hub-owner--hub-header-display.tpl.php
@@ -36,7 +36,7 @@
 <div class="row-fluid">
 	<div class="span9" style="position:relative;">
 
-		<div style="z-index:10000; height:50%; width:93%; position:absolute; left:0; bottom:0; background: rgba(0, 0, 0, 0.5); color:#fff; margin:0.5%; padding:0 3%;">
+		<div style="z-index:10000; height:50%; width:93%; position:absolute; left:0; bottom:0; background: rgba(0, 0, 0, 0.65); color:#fff; margin:0.5%; padding:0 3%;">
 			<!-- Hub title & info -->
 			<h1><?php echo $fields['title']->content; ?></h1> 
 			<span class="label ui-inline-label"><?php echo $fields['meta_og_state_state']->content; ?></span>
@@ -45,16 +45,11 @@
 		</div>
 
 		<ul class="thumbnails ui-hub-feature-img">
-
 		  <li class="span12">
-
 		    <div class="thumbnail">
-				
 				<!-- Hub img -->
 		    	<span><?php echo $fields['field_featured_hub_image']->content; ?></span>
-
 		    </div>
-
 		  </li>
 		</ul>
 
