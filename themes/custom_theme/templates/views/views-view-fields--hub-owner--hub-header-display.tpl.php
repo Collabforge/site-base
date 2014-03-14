14cc9c30c58291c55fd3f0441754d4b5f3387971
summerscope <summerscope@gmail.com>
2014-03-14 15:11:14 +1100

    More refinements to the layout to see if this might work

--- themes/custom_theme/templates/views/views-view-fields--hub-owner--hub-header-display.tpl.php
+++ themes/custom_theme/templates/views/views-view-fields--hub-owner--hub-header-display.tpl.php
@@ -36,11 +36,13 @@
 <div class="row-fluid">
 	<div class="span9" style="position:relative;">
 
-		<ul class="thumbnails">
+		<ul class="thumbnails ui-hub-feature-img">
+
 		  <li class="span12">
+
 		    <div class="thumbnail">
 		    	
-		    	<div style="z-index:10000; height:50%; width:94%; position:absolute; left:0; bottom:0; background: rgba(0, 0, 0, 0.5); color:#fff; padding:0 3%;">
+		    	<div style="z-index:10000; height:50%; width:93%; position:absolute; left:0; bottom:0; background: rgba(0, 0, 0, 0.5); color:#fff; margin:0.5%; padding:0 3%;">
 					<!-- Hub title & info -->
 					<h1><?php echo $fields['title']->content; ?></h1> 
 					<span class="label ui-inline-label"><?php echo $fields['meta_og_state_state']->content; ?></span>
@@ -50,7 +52,9 @@
 				
 				<!-- Hub img -->
 		    	<span><?php echo $fields['field_featured_hub_image']->content; ?></span>
+
 		    </div>
+
 		  </li>
 		</ul>
 
