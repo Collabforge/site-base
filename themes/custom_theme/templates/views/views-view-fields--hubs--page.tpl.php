f5aaf008c022537a29397e61d5e6f482658fe6fd
summerscope <summerscope@gmail.com>
2014-03-17 17:19:43 +1100

    Add design layer for label if a user has requested membership to a hub

--- themes/custom_theme/templates/views/views-view-fields--hubs--page.tpl.php
+++ themes/custom_theme/templates/views/views-view-fields--hubs--page.tpl.php
@@ -89,7 +89,7 @@
             if (_meta_group_is_pending_member($gid)) {
               // So the user is already requested for a membership but yet to be approved or rejected
               ?>
-              {ALREADY REQUESTED}
+              <span class="label label-info"> <i class="icon-info-sign"></i> Membership requested - pending approval </span>
               <?php
             } else {
               // The user is neither a member nor have requested for a membership
