87b75f99e8a03fdb558fe231064cdb24cbe7896b
Matt Withoos <mattwithoos@gmail.com>
2014-03-26 16:14:46 +1100

    Removed text

--- themes/collabco_theme/templates/node--blank.tpl.php
+++ themes/collabco_theme/templates/node--blank.tpl.php
@@ -4,7 +4,12 @@
  * @file
  * 
  * A template to make content of type 'blank' disappear into a rabbit hole.
+*
+* Note: Removed "This content was suppressed!" on 25/03/2014
+*
  */
+
+
 ?>
-This content was suppressed!
+
 
