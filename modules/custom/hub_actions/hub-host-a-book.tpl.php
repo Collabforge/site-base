2e32ba0d23423a4c4130c6ae26e14ad656a9f5a7
RdeBoer <rikdeboer1@yahoo.com.au>
2014-10-08 14:22:54 +1100

    Fix hub-host-a-book.tpl.php

--- modules/custom/hub_actions/hub-host-a-book.tpl.php
+++ modules/custom/hub_actions/hub-host-a-book.tpl.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * @file
-
+ * hub-host-a-book.tpl.php
  */
 ?>
 <div class="topic_host">
 	<h2><?php echo t('Create Group Book'); ?></h2>
 	<p>As a member of this group, you can add a new book which will appear within this group.</p>
-	<a href="/node/add/conversation/<?php echo $nid ?>" class="btn btn-warning"><i class="icon-comments-alt"></i> <?php echo t('Create Group Book'); ?></a>
+	<a href="<?php echo $GLOBALS['base_path'] . "node/add/book/$nid"; ?>" class="btn btn-warning"><i class="icon-comments-alt"></i> <?php echo t('Create Group Book'); ?></a>
 </div>
 
