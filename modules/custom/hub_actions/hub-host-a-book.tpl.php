261b918769dacac0f80948010cc53f41feab528a
Matt Withoos <mattwithoos@gmail.com>
2014-10-08 13:56:19 +1100

    wikibooks implemented into groups

---
+++ modules/custom/hub_actions/hub-host-a-book.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+
+ */
+?>
+<div class="topic_host">
+	<h2><?php echo t('Create Group Book'); ?></h2>
+	<p>As a member of this group, you can add a new book which will appear within this group.</p>
+	<a href="/node/add/conversation/<?php echo $nid ?>" class="btn btn-warning"><i class="icon-comments-alt"></i> <?php echo t('Create Group Book'); ?></a>
+</div>
+
--- modules/custom/hub_actions/hub-host-a-topic.tpl.php
+++ modules/custom/hub_actions/hub-host-a-topic.tpl.php
@@ -5,8 +5,8 @@
  */
 ?>
 <div class="topic_host">
-	<h2><?php echo t('Host a Sub Topic'); ?></h2>
-	<p>Owners and Co-owners of the Hub can post sub-topics of interest to members of the Hub.</p>
+	<h2><?php echo t('Host a Topic'); ?></h2>
+	<p>Members are allowed to host their own topics. Suspendisse a enim enim. Duis dapibus dictum justo nec facilisis. Donec vel semper lectus.</p>
 	<a href="/node/add/conversation/<?php echo $nid ?>" class="btn btn-warning"><i class="icon-comments-alt"></i> <?php echo t('Host a Topic'); ?></a>
 </div>
 
