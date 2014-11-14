73ed9567776b5d375f70d024fbe0146c89b4e5a7
MichaelWheldon <michael.wheldon@gmail.com>
2014-11-14 11:30:11 +1100

    title to knowledge object fix

--- themes/collabco_theme/templates/node--knowledge_object.tpl.php
+++ themes/collabco_theme/templates/node--knowledge_object.tpl.php
@@ -77,9 +77,10 @@
  * @see template_preprocess_node()
  * @see template_process()
  */
-?>
 
+?>
 <a href="/library">Library</a> > <?php print $title; ?>
+<h1><?php print $title; ?></h1>
 
 <?php if (!$is_front): ?>
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
