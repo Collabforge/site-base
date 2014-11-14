c17b9a2716d8d5915bf5ce8500987481ac6269c9
MichaelWheldon <michael.wheldon@gmail.com>
2014-11-14 12:40:17 +1100

    remove header

--- themes/collabco_theme/templates/node--knowledge_object.tpl.php
+++ themes/collabco_theme/templates/node--knowledge_object.tpl.php
@@ -80,7 +80,6 @@
 
 ?>
 <a href="/library">Library</a> > <?php print $title; ?>
-<h1><?php print $title; ?></h1>
 
 <?php if (!$is_front): ?>
   <article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
