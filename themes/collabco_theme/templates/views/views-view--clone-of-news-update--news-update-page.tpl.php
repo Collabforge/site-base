b14cf4bf760ad2de8bab9301494b05cbcf1655bf
Reshma Neerukonda <reshma_n8@yahoo.com>
2015-01-15 15:42:24 +1100

    Added news-update template

---
+++ themes/collabco_theme/templates/views/views-view--clone-of-news-update--news-update-page.tpl.php
@@ -0,0 +1,4 @@
+<div class="row">
+<?php print views_embed_view('clone_of_news_update',"recommended_news_of_news_page"); ?>
+<?php print views_embed_view('clone_of_news_update',"recommended_event_of_news_page"); ?>
+</div>
