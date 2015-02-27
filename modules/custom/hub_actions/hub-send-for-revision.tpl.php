cbd3e35ec8d8aa09351aef395568e0d84fe96d4c
Reshma Neerukonda <reshma_n8@yahoo.com>
2015-02-27 12:11:46 +1100

    Updated the labels of hub to idea

--- modules/custom/hub_actions/hub-send-for-revision.tpl.php
+++ modules/custom/hub_actions/hub-send-for-revision.tpl.php
@@ -5,6 +5,6 @@
  */
 ?>
 <div class="send-for-revision"> 
- <a href='/node/<?php echo $nid ?>/send-for-revision'><div class='button'>Send for revision</div></a>
+ <a href='/node/<?php echo $nid ?>/send-for-revision'><div class='button'>Submit for review</div></a>
 </div>
 
